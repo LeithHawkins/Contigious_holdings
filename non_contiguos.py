5965a5556f3e5a8f28d4316180156e1239b0cd8e
Corn Julio <leith.hawkins@lls.nsw.gov.au>
2017-06-21 16:22:00 +1000

    Added Search Cursor - Select by layer , Multipart to songle part,  Added count of features ,  if statement and near analysis.   AppendManagment added but not working

--- non_contiguos.py
+++ non_contiguos.py
@@ -2,25 +2,40 @@ import arcpy
 from arcpy import env
 import time
 
-#Settings
+# Settings
 baseDirectory = 'B:\\Non_Contigous_Working'
 fileworkspace = baseDirectory + '\\Non_Contiguos_Working.gdb'
 Holdings = fileworkspace + '\\Holding\\NTLLS_Holding_190617'
 
-#arcpy Settings
+# arcpy Settings
 arcpy.env.overwriteOutput = True
 env.workspace = fileworkspace
 
-
+# SearchCursor
 arcpy.MakeFeatureLayer_management(Holdings, 'Holdings_Layer')
 
-with arcpy.da.SearchCursor(Holdings, ['Holding_Reference_Number'])as Holdings_Ref_cursor:
+with arcpy.da.SearchCursor(Holdings, ['Holding_Reference_Number']) as Holdings_Ref_cursor:
     for row in Holdings_Ref_cursor:
-        start = time.time()
         refNumber = str(row[0])
-print 'Holding:' + refNumber
-arcpy.Select_analysis('Holdings_Layer', 'in_memory/holding', "Holding_Reference_Number = " + refNumber)
+        print 'Holding:' + refNumber
+        start = time.time()
+        # Select_var = 'Holding_Reference_Number = ' + refNumber
+        # arcpy.Select_analysis('Holdings_Layer', 'in_memory/holding', Select_var)
 
-arcpy.MultipartToSinglepart_management('Holdings_Layer', 'in_memory/holding_single')
+        # arcpy.MultipartToSinglepart_management('Holdings_Layer', 'in_memory/holding_single')
 
-#print('Time: ' + str(time.time() - start))
+        arcpy.SelectLayerByAttribute_management('Holdings_Layer', 'NEW_SELECTION',
+                                                "Holding_Reference_Number = " + str(row[0]))
+        arcpy.MultipartToSinglepart_management(
+            'Holdings_Layer', 'in_memory/holding_single')
+        dataset = 'in_memory/holding_single'
+        print dataset
+        output_Fc = 'B:\\Non_Contigous_Working\\Non_Contiguos_Working.gdb\\Holding\\Holding_Near_Complete'
+        result = arcpy.GetCount_management(dataset)
+        count = int(result.getOutput(0))
+        print count
+        if count > 1:
+            arcpy.Near_analysis(dataset, dataset)
+            arcpy.Append_management('in_memory/holding_single', output_Fc )
+            print 'Near Complete'
+        print('Time: ' + str(time.time() - start))
