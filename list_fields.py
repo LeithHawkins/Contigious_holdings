db6dadc037e5359af4b2a193081a1830f3297c4d
Corn Julio <leith.hawkins@lls.nsw.gov.au>
2017-06-22 16:27:41 +1000

    Added Function for calculating largest value and have started working on calculating the fields

--- list_fields.py
+++ list_fields.py
@@ -8,28 +8,5 @@ for field in fields:
           .format(field.name))
 
 
-'Holding_Name'
-'Holding_Location_Address'
-"Local_Land_Services_Region_Id
-Local_Land_Services_Region_Name
-RLPB_Board_Name
-Property_Identification_Code
-Occupier_Id
-Occupier_Full_Name
-Occupier_Mailing_Address
-Occupier_Home_Phone
-Occupier_Mobile_Phone
-Occupier_Email_Address
-Total_Area
-Is_Rateable_Indicator
-Rateable_Area
-Nominal_Notional_Carrying_Cap
-SHAPE_STArea__
-SHAPE_STLength__
-ORIG_FID
-'NEAR_FID
-'NEAR_DIST
-'Shape_Length
-'Shape_Area'
 
 
--- non_contiguos.py
+++ non_contiguos.py
@@ -4,7 +4,7 @@ import time
 # Settings
 baseDirectory = 'B:\\Non_Contigous_Working'
 fileworkspace = baseDirectory + '\\Non_Contiguos_Working.gdb'
-Holdings = fileworkspace + '\\Holding\\NTLLS_Holding_190617'
+Holdings = fileworkspace + '\\test_Output\\Holding_test'
 # arcpy Settings
 arcpy.env.overwriteOutput = True
 env.workspace = fileworkspace
@@ -16,22 +16,43 @@ with arcpy.da.SearchCursor(Holdings, ['Holding_Reference_Number']) as Holdings_R
     for row in Holdings_Ref_cursor:
         refNumber = str(row[0])
         print 'Holding:' + refNumber
-        holding = 'Holding:' + refNumber
+        holding = 'Holding' + refNumber
         start = time.time()
         arcpy.SelectLayerByAttribute_management('Holdings_Layer', 'NEW_SELECTION',
                                                 "Holding_Reference_Number = " + str(row[0]))
-        arcpy.MultipartToSinglepart_management('Holdings_Layer', 'in_memory/holding_single')
+        arcpy.MultipartToSinglepart_management(
+            'Holdings_Layer', 'in_memory/holding_single')
         dataset = 'in_memory/holding_single'
+        dataset_dissolve = 'in_memory/holding_dissolve'
+        out_Table = 'in_memory/output_table'
+        print out_Table
 
         result = arcpy.GetCount_management(dataset)
         count = int(result.getOutput(0))
         if count > 1:
             test_Output = 'B:\\Non_Contigous_Working\\Non_Contiguos_Working.gdb'
-            out_Table = test_Output + '\\h_' + refNumber
-            arcpy.GenerateNearTable_analysis(dataset, dataset, out_Table,'','NO_LOCATION', 'NO_ANGLE' ,'All')
-            output_Fc = 'B:\\Non_Contigous_Working\\Non_Contiguos_Working.gdb\\Holding\\Holding_Near_Complete'
+            large_Output = 'B:\\Non_Contigous_Working\\Non_Contiguos_Working.gdb\\test_Output\\'
 
-            #arcpy.FeatureClassToFeatureClass_conversion(dataset, test_Output, '\\holding_'+ refNumber )
-            #arcpy.Append_management('in_memory/holding_single', output_Fc, 'NO_TEST','','')
-            print 'Near Complete'
-        print('Time: ' + str(time.time() - start))
+            arcpy.GenerateNearTable_analysis(
+                dataset, dataset, out_Table, '', 'NO_LOCATION', 'NO_ANGLE', 'All')
+
+            def unique_values(table, Field):
+                with arcpy.da.SearchCursor(table, [Field]) as cursor:
+                    return sorted({row[0] for row in cursor})
+            #unique_values(out_Table ,'NEAR_DIST')
+            large = max(unique_values(out_Table, 'NEAR_DIST'))
+            if large > 200:
+                field = 'Max_Distance'
+                arcpy.Dissolve_management(dataset,dataset_dissolve, ['Holding_Reference_Number'])
+                arcpy.AddField_management(dataset_dissolve, field, 'DOUBLE', '', '','','')
+                while row:
+                    row.setValue(field, row.getValue(large))
+                    cursor.updateRow(row)
+                    row = cursor.next()
+                    pass
+                #     arcpy.FeatureClassToFeatureClass_conversion(dataset, large_Output, 'holding_'+ refNumber )
+
+                #output_Fc = 'B:\\Non_Contigous_Working\\Non_Contiguos_Working.gdb\\Holding\\Holding_Near_Complete'
+                #arcpy.Append_management('in_memory/holding_single', output_Fc, 'NO_TEST','','')
+                print'Near Complete'
+    print('Time: ' + str(time.time() - start))
